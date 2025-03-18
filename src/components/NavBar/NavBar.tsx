b113aee1c011c5e09e9467259d44c0f7cfdae7e9
j-pendlebury <j_blaut@hotmail.com>
2025-03-18 23:35:28 +0000

    nav start

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -4,7 +4,16 @@ import classes from './NavBar.module.scss';
 // type Props = {};
 
 const NavBar = () => {
-  return <div className={classes.navBarWrapper}>NavBar</div>;
+  return <div className={classes.navBarWrapper}>
+    <div className={classes.navBarInner}>
+      <ul>
+        <li><a href={'/'}>Sets</a></li>
+        <li><a href={'/'}>Instructions</a></li>
+        <li><a href={'/'}>Wishlist</a></li>
+        <li className={classes.newBtn}><a href={'/'}>Add new</a></li>
+      </ul>
+    </div>
+  </div>;
 };
 
 export default NavBar;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app';
+import '@/styles/main.scss';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
