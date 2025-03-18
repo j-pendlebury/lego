0fa381ffb34dcdf14b5584d5b67481fd3c0c63da
jamie <j_blaut@hotmail.com>
2025-03-18 18:23:45 +0000

    cleanup and setup before components

---
+++ src/components/NavBar/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './NavBar.module.scss';
+
+// type Props = {};
+
+const NavBar = () => {
+  return <div className={classes.navBarWrapper}>NavBar</div>;
+};
+
+export default NavBar;
