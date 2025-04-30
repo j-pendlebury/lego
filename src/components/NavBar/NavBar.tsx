e98a19550bf1afee54ad22fb069164ee56665173
j-pendlebury <j_blaut@hotmail.com>
2025-04-30 22:19:01 +0100

    add navbar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -7,9 +7,9 @@ const NavBar = () => {
   return <div className={classes.navBarWrapper}>
     <div className={classes.navBarInner}>
       <ul>
-        <li><a href={'/'}>Sets</a></li>
-        <li><a href={'/'}>Instructions</a></li>
-        <li><a href={'/'}>Wishlist</a></li>
+        <li className={classes.navLink}><a href={'/'}>Sets</a></li>
+        <li className={classes.navLink}><a href={'/'}>Instructions</a></li>
+        <li className={classes.navLink}><a href={'/'}>Wishlist</a></li>
         <li className={classes.newBtn}><a href={'/'}>Add new</a></li>
       </ul>
     </div>
